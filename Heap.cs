304f3ab71f7dc60408aa3edc18f2f410fb1f799a
Nathan Gonçalves <nathan@liberaai.com>
2024-03-27 11:38:15 -0300

    Metodos Heap

--- Heap.cs
+++ Heap.cs
@@ -8,9 +8,32 @@ public class Heap
 
     public Heap(int capacidadeMax)
     {
-        vetor = new List<Elemento>(10);
+        vetor = new List<Elemento>(capacidadeMax) { };
     }
 
+    public static Heap ConstroiHeap(List<Elemento> vetor)
+    {
+        var heap = new Heap(vetor.Count);
+        for (int i = vetor.Count / 2; i >= 1; i--)
+        {
+            heap.Heapify(vetor, i);
+        }
+        return heap;
+    }
+
+    public static void HeapSort(List<Elemento> vetor)
+    {
+        var heap = ConstroiHeap(vetor); 
+        for (int i = vetor.Count; i > 1; i--)
+        {
+            Elemento temp = vetor[1];
+            vetor[1] = vetor[i];
+            vetor[i] = temp;
+            heap.Heapify(vetor, i);
+        }
+    }
+
+    // FORMULAS MODIFICADAS PARA CONTAR COM O INDICE 0 DO VETOR
     private int Pai(int i)
     {
         return i / 2;
@@ -31,12 +54,42 @@ public class Heap
         if (vetor.Count == vetor.Capacity)
             throw new Exception("Heap cheio");
         int i = vetor.Count;
-        while(i > 1 && prioridade < vetor[i].prioridade)
+        while (i > 1 && prioridade < vetor[i].prioridade)
         {
             vetor[i] = vetor[Pai(i)];
-            i = Pai(i);
+            i = Pai(i); 
         }
-        vetor[i] = new Elemento(elemento, prioridade);
+        vetor.Insert(i, new Elemento(elemento, prioridade));
+    }
+
+    private void Heapify(List<Elemento> vetor, int i)
+    {
+        int esq = FilhoEsquerda(i);
+        int dir = FilhoDireita(i);
+        int maior = i;
+        if (esq < vetor.Count && vetor[esq].prioridade > vetor[i].prioridade)
+            maior = esq;
+        if (dir < vetor.Count && vetor[dir].prioridade > vetor[maior].prioridade)
+            maior = dir;
+        if (maior != i)
+        {
+            Elemento temp = vetor[i];
+            vetor[i] = vetor[maior];
+            vetor[maior] = temp;
+            Heapify(vetor, maior);
+        }
+        this.vetor = vetor;
+    }
+
+    public Elemento ExtrairMax()
+    {
+        if (vetor.Count == 0)
+            throw new Exception("Heap vazio");
+        Elemento max = vetor[1];
+        vetor[1] = vetor[vetor.Count - 1];
+        vetor.RemoveAt(vetor.Count - 1);
+        Heapify(vetor, 1);
+        return max;
     }
 }
 
@@ -49,5 +102,10 @@ public class Elemento
     {
         this.valor = valor;
         this.prioridade = prioridade;
+    }
+
+    public override string ToString()
+    {
+        return $"Valor: {valor}, Prioridade: {prioridade}";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using FilaDePrioridade;
 
 Heap heap = new Heap(10);
+
+Console.WriteLine("Inserindo elementos na fila de prioridade...");
+
+while (true)
+{
+    Console.WriteLine("Digite o elemento a ser inserido:");
+    int elemento = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a prioridade do elemento:");
+    int prioridade = int.Parse(Console.ReadLine());
+    heap.Inserir(elemento, prioridade);
+    Console.WriteLine("Elemento inserido com sucesso!");
+    Console.WriteLine("Deseja inserir outro elemento? (s/n)");
+    if (Console.ReadLine().ToLower() == "n")
+        break;
+}
