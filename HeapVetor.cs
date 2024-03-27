cd9e78d04ccd8394f5e8bef9b58d85177703a9d1
Nathan Gonçalves <nathan@liberaai.com>
2024-03-27 17:02:28 -0300

    Progress

--- HeapVetor.cs
+++ HeapVetor.cs
@@ -8,7 +8,7 @@ public static class HeapVetor
 
     public static void ConstroiHeap(this Elemento[] vetor)
     {
-        for (int i = vetor.Length / 2; i >= 1; i--)
+        for (int i = TamanhoVetor(vetor) / 2; i >= 1; i--)
         {
             Heapify(vetor, i);
         }
@@ -19,20 +19,20 @@ public static class HeapVetor
         ConstroiHeap(vetor); 
         for (int i = TamanhoHeap(vetor); i > 1; i--)
         {
-            Elemento temp = vetor[1];
-            vetor[1] = vetor[i];
+            Elemento temp = vetor[0];
+            vetor[0] = vetor[i];
             vetor[i] = temp;
             Heapify(vetor, i);
         }
     }
 
-    private static int Pai(int i) => i / 2;
+    private static int Pai(int i) => (i - 1) / 2;
 
-    private static int FilhoEsquerda(int i) => 2 * i;
+    private static int FilhoEsquerda(int i) => 2 * i + 1;
 
-    private static int FilhoDireita(int i) => 2 * i + 1;
+    private static int FilhoDireita(int i) => 2 * i + 2;
 
-    private static int TamanhoHeap(Elemento[] vetor) => vetor.Skip(1).Count();
+    private static int TamanhoHeap(Elemento[] vetor) => vetor.Count();
     private static int TamanhoVetor(Elemento[] vetor) => vetor.Length;
 
     public static void Inserir(this Elemento[] vetor, Elemento elemento)
@@ -40,8 +40,8 @@ public static class HeapVetor
         if (TamanhoHeap(vetor) == TamanhoVetor(vetor))
             throw new Exception("Heap cheio");
 
-        int i = TamanhoHeap(vetor) + 1;
-        while (i > 1 && elemento.Prioridade > vetor[i].Prioridade)
+        int i = TamanhoHeap(vetor);
+        while (i > 0 && elemento.Prioridade > vetor[i].Prioridade)
         {
             vetor[i] = vetor[Pai(i)];
             i = Pai(i); 
@@ -72,18 +72,18 @@ public static class HeapVetor
         if (TamanhoHeap(vetor) == 0)
             throw new Exception("Heap vazio");
 
-        Elemento max = vetor[1];
-        vetor[1] = vetor[TamanhoHeap(vetor)];
+        Elemento max = vetor[0];
+        vetor[0] = vetor[TamanhoHeap(vetor)];
         vetor[TamanhoHeap(vetor)] = null;
 
-        Heapify(vetor, 1);
+        Heapify(vetor, 0);
         return max;
     }
 
     public static void PrintHeap(Elemento[] heap)
     {
         int maxLevel = NivelRaiz(heap.Length);
-        for (int level = 0; level <= maxLevel; level++)
+        for (int level = 1; level <= maxLevel; level++)
         {
             int nodes = (int)Math.Pow(2, level);
             string spacing = new string(' ', (int)Math.Pow(2, maxLevel - level) - 1);
@@ -103,7 +103,7 @@ public static class HeapVetor
 
     private static int NivelRaiz(int n)
     {
-        return (int)Math.Ceiling(Math.Log(n, 2));
+        return (int)Math.Ceiling(Math.Log(n + 1, 2)) - 1;
     }
 }
 
